bd81d8c4bd81a28e097fc22848d039d299a13853
Alvin Alic <alvin.alic@gmail.com>
2024-11-01 13:51:47 -0500

    Add method so computer can generate the initial guess

--- lib/computer.rb
+++ lib/computer.rb
@@ -1,7 +1,7 @@
 require_relative 'Player.rb'
 class Computer < Player
 
-  attr_accessor :secret_code
+  attr_accessor :secret_code, :guess
   def initialize
     self.secret_code = generate_code
   end
@@ -15,6 +15,26 @@ class Computer < Player
     end
     code
   end
+
+  def initial_guess
+    guess = []
+    while guess.length < 4
+      current_guess = COLOR_OPTIONS[random_number_generator]
+      guess.push(current_guess) unless guess.include?(current_guess)
+    end
+    @guess = guess
+  end
+
+  # def guess_code(hints)
+  #   hints.each_with_index do |hint, index|
+  #     if hint == "Green"
+  #       next
+  #       elsif hint == "White"
+          
+  #     end
+      
+  #   end
+  # end
 
   private 
 
